e6a9c0c0d6b592aa1da8f9dc9091314bcdc1c0ed
Michael Dinh <michaeldinh1994@gmail.com>
2020-02-19 22:28:03 -0800

    Deploy file update

--- deploy.config.js
+++ deploy.config.js
@@ -1,1 +1,19 @@
-module.exports = {"apps":{"name":"appointment.usemy.app","script":"npm run start:myproxy << /home/myproxy/.pm2/logs/appointment.usemy.app-out.log","instances":1,"autorestart":true,"watch":false,"max_memory_restart":"1G","env_production":{"NODE_ENV":"production","PORT":3051,"ADMIN":"A3uPmSvEs5H7FfgmjCUsqD3vsshwuRMwezneqXdA","WORKPATH":"/home/myproxy"},"error_file":"/home/myproxy/.pm2/logs/appointment.usemy.app-err.log","merge_logs":true}}
+module.exports = {
+  apps: {
+    name: "appointment.usemy.app",
+    script:
+      "npm run start:myproxy << /home/myproxy/.pm2/logs/appointment.usemy.app-out.log",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3051,
+      ADMIN: "A3uPmSvEs5H7FfgmjCUsqD3vsshwuRMwezneqXdA",
+      WORKPATH: "/home/myproxy"
+    },
+    error_file: "/home/myproxy/.pm2/logs/appointment.usemy.app-err.log",
+    merge_logs: true
+  }
+};
